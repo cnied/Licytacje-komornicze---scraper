8fad47c5e9008b6832b851d46eb93ef089d8baf8
Cezary Niedzialek <czniedzialek@gmail.com>
2026-01-04 16:47:37 +0100

    Change of local llama to Gemini API

--- bs4data.py
+++ bs4data.py
@@ -1,50 +1,45 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-OLLAMA_URL = "http://localhost:11434/v1/chat/completions"
-MODEL = "llama3.1:8b"
-prompt = f"""
-Masz poniższy tekst ogłoszenia o licytacji komorniczej w Polsce.
 
-Wyciągnij dane i zwróć WYŁĄCZNIE poprawny JSON
-z polami:
-- suma_oszacowania (int, PLN)
-- cena_wywolawcza (int, PLN)
-- data_licytacji (YYYY-MM-DD)
-- godzina_licytacji (HH:MM)
-- adres (string)
-- sygnatura (string)
-- nr ksiegi_wieczystej (string)
-
-Jeśli czegoś nie ma, użyj null.
-
-TEKST:
-\"\"\"
-{text}
-\"\"\"
-"""
 
 url = "https://licytacje.komornik.pl/Notice/Details/664034"
-
-payload = {
-    "model": MODEL,
-    "prompt": prompt,
-    "stream": False,
-    "options": {
-        "temperature": 0
-    }
-}
+load_dotenv(".env")
+client = genai.Client()
 
 def fetch_auction_data(url):
     html = requests.get(url,timeout=10).text
     soup = BeautifulSoup(html, "html.parser")
-    response = requests.post(OLLAMA_URL, json=payload, timeout=120)
-    result = response.json()["response"]
-    data = json.loads(result)
     return soup.get_text(separator="\n", strip=True)
 
 
+def ai_response(soup):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        config=types.GenerateContentConfig(
+            # Wymuszenie formatu JSON
+            response_mime_type="application/json",
+            system_instruction="""
+            Wyciągnij dane z ogłoszenia o licytacji komorniczej. 
+            Zwróć JSON z polami:
+            - suma_oszacowania (int)
+            - cena_wywolawcza (int)
+            - data_licytacji (format YYYY-MM-DD)
+            - godzina_licytacji (format HH:MM)
+            - adres (string)
+            - sygnatura (string)
+            - nr_ksiegi_wieczystej (string)
+            """
+        ),
+        contents=soup
+    )
+    return json.loads(response.text)
+
+
 if __name__ == "__main__":
-    print(json.dumps(data, indent=2, ensure_ascii=False))
+    print(ai_response(fetch_auction_data(url)))
 
